9a807cb439cab7eab111f7c3b7db06f03db4d863
tranduytung <tranduytung1994@gmail.com>
2016-04-28 07:29:13 +0700

    add gem devise for model cluster

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -1,6 +1,10 @@
 class Cluster < ActiveRecord::Base
-	has_many :examinees
-	validates :name, presence: true
-	validates :code, presence: true
-	validates :year, presence:true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :examinees
+  validates :name, presence: true, length: {maximum: 150}
+  validates :code, presence: true, length: {is: 8}
+  validates :year, presence:true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :clusters, :controllers => {
+    :passwords => "clusters/passwords",
+    :confirmations => "clusters/confirmations",
+    :registrations => "clusters/registrations",
+    :sessions => "clusters/sessions"
+  }
   root "static_pages#home"
 
   get "home" => "static_pages#home"
