c56d727d56da9fb3da894716864ca2dc742dd8b9
tranduytung <tranduytung1994@gmail.com>
2016-05-12 14:42:21 +0700

    edit cluster with other information

--- app/controllers/clusters/registrations_controller.rb
+++ app/controllers/clusters/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Clusters::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -23,18 +23,18 @@ class Clusters::RegistrationsController < Devise::RegistrationsController
   end
 
   # DELETE /resource
-  def destroy
-    super
-  end
+  # def destroy
+  #   super
+  # end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
   # cancel oauth signing in/up in the middle of the process,
   # removing all OAuth session data.
-  def cancel
-    super
-  end
+  # def cancel
+  #   super
+  # end
 
   # protected
 
@@ -45,7 +45,10 @@ class Clusters::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email,
+      :name, :address, :year, :phone, :test_day,:map, :current_password,
+      :password, :password_confirmation) }
   end
 
   # The path used after sign up.
