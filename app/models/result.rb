60a2d2342099e496a03da735d7d5490f59920f56
tranduytung <tranduytung1994@gmail.com>
2016-05-18 13:53:45 +0700

    fix find_exam to send

--- app/models/result.rb
+++ app/models/result.rb
@@ -9,17 +9,6 @@ class Result < ActiveRecord::Base
   validates :history, presence: true
   validates :geography, presence: true
 
-  def find_exam exam_name
-    return self.math if exam_name == "math"
-    return self.literature if exam_name == "literature"
-    return self.english if exam_name == "english"
-    return self.physical if exam_name == "physical"
-    return self.chemistry if exam_name == "chemistry"
-    return self.biological if exam_name == "biological"
-    return self.history if exam_name == "history"
-    return self.geography if exam_name == "geography"
-  end
-
   def graduate_score_caculation
     @total = 0
     @group_graduated_exam = GroupGraduatedExam. find_by(
@@ -30,9 +19,8 @@ class Result < ActiveRecord::Base
     @exam_graduated_all << (Exam.find_by id: @group_graduated_exam.exam_id_3)
     @exam_graduated_all << (Exam.find_by id: @group_graduated_exam.exam_id_4)
     @exam_graduated_all.each do |exam|
-      @total += self.find_exam exam.name.downcase
+      @total += self.send exam.name.downcase
     end
     return (@total.fdiv(@exam_graduated_all.length))
   end
-
 end
