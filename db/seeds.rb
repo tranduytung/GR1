6135b02cad024710ae5a837d7ef404e356c56870
tranduytung <tranduytung1994@gmail.com>
2017-02-18 21:02:51 +0700

    fix major group exams

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
-#   load seed
-# end
-load "/home/duytung/GR/GR1/db/seeds/9_register.rb"
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
+  load seed
+end
+# load "/home/duytung/GR/GR1/db/seeds/9_register.rb"
